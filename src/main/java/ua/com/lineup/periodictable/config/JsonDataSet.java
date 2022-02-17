8c832198de0387a6305341fb40a9674a36d494c4
bitiukovaleksei <bitiukovaleksei@ukr.net>
2022-02-17 12:58:01 +0200

    clear unused code

--- src/main/java/ua/com/lineup/periodictable/config/JsonDataSet.java
+++ src/main/java/ua/com/lineup/periodictable/config/JsonDataSet.java
@@ -1,27 +1,22 @@
 package ua.com.lineup.periodictable.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import ua.com.lineup.periodictable.service.ElementService;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 
 @Component
 public class JsonDataSet {
 
     private final ElementService elementService;
 
-    @Value("classpath:periodic_table.json")
-    Resource resourceFile;
 
     public JsonDataSet(ElementService elementService) {
         this.elementService = elementService;
     }
 
     @PostConstruct
-    void uploadDataSet() throws IOException {
+    void uploadDataSet() {
         elementService.uploadStartDataSet("/periodic_table.json");
     }
 }
